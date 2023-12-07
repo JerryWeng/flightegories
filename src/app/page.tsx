13f588c70a87adf0044f859d7984a9644b9195f6
jerryweng <mcjerry168@gmail.com>
2023-12-07 15:39:15 -0500

    hover animations, cleaner look, created reserve page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import Logo from "../public/images/logo.png";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -9,37 +10,55 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
-        <div className={styles.grid}>
-          {!session ? (
-            <>
-              <Link href="/login" className={styles.card}>
-                <h2>
-                  Login <span>-&gt;</span>
-                </h2>
-              </Link>
+        <img src={Logo.src} alt="logo" className={styles.logo} />
+        <div className={styles.about}>
+          <div className={styles.aboutContent}>
+            <h2 className={styles.aboutHeading}>About</h2>
+            <p className={styles.aboutText}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+              dolorem quam sint molestiae commodi beatae neque repudiandae at
+              odit debitis?
+            </p>
+          </div>
+          <div className={styles.grid}>
+            {!session ? (
+              <>
+                <Link href="/login" className={styles.card}>
+                  <h2 className={styles.cardLink}>
+                    <span className={styles.cardText}>Login</span>
+                    <span className={styles.arrow}>-&gt;</span>
+                  </h2>
+                </Link>
 
-              <Link href="/register" className={styles.card}>
-                <h2>
-                  Register <span>-&gt;</span>
-                </h2>
-              </Link>
-            </>
-          ) : (
-            <>
-              <button 
-                className={styles.card}
-                onClick={() => {
-                  signOut();
-                }}
-              >
-                Logout
+                <Link href="/register" className={styles.card}>
+                  <h2 className={styles.cardLink}>
+                    <span className={styles.cardText}>Register</span>
+                    <span className={styles.arrow}>-&gt;</span>
+                  </h2>
+                </Link>
+              </>
+            ) : (
+              <>
+                <button
+                  className={styles.card}
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  Logout
                 </button>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
       </div>
       <div className={styles.reserve}>
-        <a href="http://" target="_blank" rel="noopener noreferrer" className={styles.reserveLink}>
+        <a
+          href="/reserve"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.reserveLink}
+        >
           Reserve Flights
         </a>
       </div>
